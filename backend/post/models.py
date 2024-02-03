ceb76a279a264ae790dacc1529c6cb199b463a7b
AbdelilahChahbouni <abdelchahbouni@live.fr>
2024-02-03 17:09:39 +0100

    create post , comment , like models

--- backend/post/models.py
+++ backend/post/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(max_length=1000)
+    author = models.ForeignKey(User , on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    content = models.TextField(max_length=1000)
+    author = models.ForeignKey(User , on_delete=models.models.CASCADE)
+    post = models.ForeignKey(Post , on_delete=models.Case)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.author)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post , on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Like by {self.user.username} on post {self.post.id}" 
