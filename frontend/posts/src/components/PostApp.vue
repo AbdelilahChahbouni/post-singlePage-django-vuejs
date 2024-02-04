8f7f6fc1540197799611d293d218029f57015706
AbdelilahChahbouni <abdelchahbouni@live.fr>
2024-02-04 19:38:33 +0100

    add external file css

--- frontend/posts/src/components/PostApp.vue
+++ frontend/posts/src/components/PostApp.vue
@@ -81,6 +81,7 @@
 
 
 <script>
+    import '@/assets/styles/style.css'
     export default {
         name : 'post-app',
         data() {
@@ -105,137 +106,9 @@
 
 </script>
 
-<style>
-    .post-card {
-        background-color: #eee;
-        border-radius: 5px;
-        margin-top: 100px;
-        display: block;
-        height: 100vh;
-        
-}
-.post-card h1 {
-    color : blueviolet;
-    font-size: 30px;
-    text-align: center;
-    padding-top: 20px;
-    padding-bottom: 20px;
-}
-.post-body {
-    background-color: #9999;
-    width: 55%;
-    margin: auto;
-    padding: 10px;
-    border-radius: 7px;
-    position: relative;
-}
-.post-body .post-user {
-    display: flex;
-    align-items: center;
-}
-.post-user img {
-    width: 80px;
-    height: 80px;
-    border-radius: 50%;
-    margin-right: 10px
-}
-.post-user h2 {
-    font-size: 18px;
-    margin-top: 10px;
-
-}
-.post-content .post-title {
-    margin-top: 23px;
-    font-size: 20px;
-    color: #777;
-
-}
-
-.post-content p{
-    font-size : 18px;
-    line-height: 1.6;
-    color: #444
-
-}
-.post-body .like-section {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-top: 20px;
-    border-top: 1px solid #eee;
-    border-bottom: 1px solid #eee;
-    padding-top: 10px;
-    padding-bottom: 10px;
-}
-.like-section i {
-    color : blue;
-    margin-right: 10px;
-    font-size: 20px;
-}
-.like-section i span {
-    display: inline-block;
-    margin-left: 10px;
-    color: blue;
-    font-size: 15px;
-}
-.like-section > span {
-    color: blue;
-}
-.post-body .post-actions {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding-top: 10px ;
-}
-
-.post-actions a {
-    text-decoration: none;
-    color : blue;
-    
-}
-.post-actions a i {
-    font-size: 20px;
-    display: inline-block;
-    margin-right: 10px;
-}
-.comment-content .add-comment {
-    display: flex;
-    justify-content: space-between;
-}
-.comment-content .add-comment input {
-    border-radius: 8px;
-    color:#777;
-    outline: none;
-    border: none;
-    padding-left: 10px;
-
-}
-.my-card {
-    width: 200px;
-    height: 400px;
-    background-color: #999;
-    color:#444
-}
-.like-users {
-    position: absolute;
-    top: 200px;
-    right:-200px;
-    background-color:#999 ;
-    color: white;
-    
-}
-.list-comments {
-    background-color: #ccc;
-    border-radius: 10px;
-    margin-top: 10px;
-    padding: 10px;
-}
-.list-comments p {
-    margin-top: 15px;
-    padding-left: 20px;
-    color: #444;
-    font-size: 18px;
-}
 
 
-</style>
+
+
+
+
